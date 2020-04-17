554fa104bcce13c40bce9a88968e33563c0fced0
hannn0919 <44270736+hannn0919@users.noreply.github.com>
2020-04-18 07:41:55 +0800

    Initial Commit

---
+++ 2020_iOS_bonus2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  2020_iOS_bonus2
+//
+//  Created by Hannn on 2020/4/18.
+//  Copyright © 2020 Hannn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
